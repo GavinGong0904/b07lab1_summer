b7bc557d8985c234619f619c9c9e676e391de227
Gequan Gong <gonggequan@gmail.com>
2024-06-02 22:53:31 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -14,3 +14,50 @@ public class Driver {
 			System.out.println("1 is not a root of s");
 		}
 }
+
+
+import java.io.File;
+import java.io.IOException;
+
+public class Driver {
+    public static void main(String[] args) {
+        Polynomial p = new Polynomial();
+        System.out.println(p.evaluate(3));
+        
+        double[] c1 = {6, 5};
+        int[] p1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, p1);
+        System.out.println(p1.evaluate(3);
+
+        double[] c2 = {-2, -9};
+        int[] p2 = {1, 4};
+        Polynomial p2 = new Polynomial(c2, p2);
+        System.out.println(p2.evaluate(3);
+
+        Polynomial s = p1.add(p2);
+        System.out.println(s.evaluate(0.1));
+
+        if (s.hasRoot(1)) {
+            System.out.println("1 is a root of s");
+        } else {
+            System.out.println("1 is not a root of s");
+        }
+
+        Polynomial t = p1.multiply(p2);
+        System.out.println(t.evaluate(3));
+
+        try {
+            Polynomial polynomialText = new Polynomial(new File("polynomial.txt"));
+            System.out.println("pFromFile(3) = " + polynomialText.evaluate(3));
+        } catch (IOException e) {
+            System.out.println("Error reading polynomial from file: " + e.getMessage());
+        }
+
+        try {
+            s.saveToFile("output_polynomial.txt");
+            System.out.println("Polynomial saved to output_polynomial.txt");
+        } catch (IOException e) {
+            System.out.println("Error saving polynomial to file: " + e.getMessage());
+        }
+    }
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,61 +1,197 @@
-/** CSCB07 Lab 1
- * Develop class Polynomial as follows:
- * i. It has one field representing the coefficients of the polynomial using an array of
- * double. A polynomial is assumed to have the form a_0 + a_1x + ... + a_nx^n.
- * For example, the polynomial 6 - 2x + 5x^2 would be represented using the
- * array [6, -2, 0, 5]
- * ii. It has a no-argument constructor that sets the polynomial to zero (i.e. the
- * corresponding array would be [0])
- * iii. It has a constructor that takes an array of double as an argument and sets the
- * coefficients accordingly
- * iv. It has a method named add that takes one argument of type Polynomial and
- * returns the polynomial resulting from adding the calling object and the argument
- * v. It has a method named evaluate that takes one argument of type double
- * representing a value of x and evaluates the polynomial accordingly. For example,
- * if the polynomial is 6 - 2x + 5x^2 and evaluate(-1) is invoked, the result should
- * be 3.
- * vi. It has a method named hasRoot that takes one argument of type double and
- * determines whether this value is a root of the polynomial or not. Note that a root
- * is a value of x for which the polynomial evaluates to zero.
+/** CSCB07 Lab 2
+ * Modify Polynomial.java as follows:
+ * a. Replace the array representing the coefficients by two arrays: one representing the nonzero
+ * coefficients (of type double) and another one representing the corresponding
+ * exponents (of type int). For example, the polynomial 6 - 2x + 5x^3 would be represented
+ * using the arrays [6, -2, 5] and [0, 1, 3]
+ * b. Update the existing methods accordingly
+ * c. Add a method named multiply that takes one argument of type Polynomial and returns
+ * the polynomial resulting from multiplying the calling object and the argument. The
+ * resulting polynomial should not contain redundant exponents.
+ * d. Add a constructor that takes one argument of type File and initializes the polynomial
+ * based on the contents of the file. You can assume that the file contains one line with no
+ * whitespaces representing a valid polynomial. For example: the line 5-3x2+7x8
+ * corresponds to the polynomial 5 - 3x^2 + 7x^8
+ * Hint: you might want to use the following methods: split of the String class, parseInt of
+ * the Integer class, and parseDouble of the Double class
+ * e. Add a method named saveToFile that takes one argument of type String representing a
+ * file name and saves the polynomial in textual format in the corresponding file (similar to
+ * the format used in part d)
+ * f. You can add any supplementary classes/methods you might find useful
  */
+import java.util.HashSet;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class Polynomial {
 	double[] coefficient;
+	int[] power;
 	
 	public Polynomial() {
 	    this.coefficient = new double[]{0};
+	    this.power = new int[]{0};
 	}
 	
-	public Polynomial(double[] coefficient) {
+	public Polynomial(double[] coefficient, int[] power) {
 	    this.coefficient = coefficient;
+	    this.power = power;
 	}
-	
-	public Polynomial add(Polynomial p) {
-	    int len = Math.max(this.coefficient.length, p.coefficient.length);
-	    double[] result = new double[len];
-	    
-	    for (int i = 0; i < len; i++) {
-	        double coeff_t = 0;
-	        double coeff_p = 0;
-	        if (i < this.coefficient.length) {
-	        	coeff_t = this.coefficient[i];
+
+	public int getAddLength(Polynomial polynomial) {
+		int length = 0, i = 0, j = 0;
+		while (i < this.power.length && j < polynomial.power.length) {
+			if (this.power[i] == polynomial.power[j]) {
+				length++;
+				i++;
+				j++;
+			} else if (this.power[i] < polynomial.power[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+		return length;
+	}
+
+	public Polynomial add(Polynomial polynomial) {
+		int length = getAddLength(polynomial);
+		double[] resultCoefficient = new double[length];
+	    int[] resultPower = new int[length];
+	    int i = 0, j = 0;
+	    for (int k = 0; k < length; k++) {
+	        if (i == this.power.length) {
+	        	resultCoefficient[k] = polynomial.coefficient[j];
+	        	resultPower[k] = polynomial.power[j];
+	        	j++;
+	        } else if (j == polynomial.power.length) {
+	        	resultCoefficient[k] = this.coefficient[i];
+	        	resultPower[k] = this.power[i];
+	        	i++;
+	        } else {
+	        	resultCoefficient[k] = this.coefficient[i] + polynomial.coefficient[j];
+	        	resultPower[k] = polynomial.power[i];
+	        	i++;
+	        	j++;
 	        }
-	        if (i < p.coefficient.length) {
-	        	coeff_p = p.coefficient[i];
-	        }
-	        result[i] = coeff_t + coeff_p;
 	    }
-	    return new Polynomial(result);
+	    return new Polynomial(resultCoefficient, resultPower);
 	}
 	
 	public double evaluate(double x) {
         double result = 0;
         for (int i = 0; i < coefficient.length; i++) {
-            result += coefficient[i] * Math.pow(x, i);
+            result += this.coefficient[i] * Math.pow(x, this.power[i]);
         }
         return result;
     }
 	
 	public boolean hasRoot(double x) {
         return evaluate(x) == 0;
+    }
+
+	public Polynomial multiply(Polynomial polynomial) {
+		HashSet<Integer> powerSet = new HashSet<>();
+		for (int i = 0; i < this.power.length; i++) {
+			for (int j = 0; j < polynomial.power.length; j++) {
+				powerSet.add(this.power[i] + polynomial.power[j]);
+			}
+		}
+		int[] resultPower = new int[powerList.size()];
+		for (int i = 0; i < powerList.size(); i++) {
+		    resultPower[i] = powerList.get(i);
+		}
+		Collections.sort(powerList);
+		int[] resultPower = list.toArray(new int[0]);
+		double[] resultCoefficient = new double[resultPower.length];
+		int k = 0;
+		double l = 0;
+		for (int i = 0; i < this.power.length; i++) {
+			for (int j = i; j < polynomial.power.length; j++) {
+				k = this.power[i] + polynomial.power[j];
+				l = this.coefficient[i] * polynomial.coefficient[j];
+				for (int m = 0; m < resultPower.length; m++) {
+					if (resultPower[m] == k) {
+						resultCoefficient[m] += l;
+					}
+				}
+			}
+		}
+		return new Polynomial(resultCoefficient, resultPower);
+	}
+	
+	public Polynomial(File file) {
+        // Get the string from file.
+		Scanner scanner = new Scanner(file);
+        String polynomialStr = scanner.nextLine();
+        scanner.close();
+        // Deal with the string.
+		String[] part = polynomialStr.split("+-");
+		List<Double> coefficientList = new ArrayList<>();
+		List<Integer> powerList = new ArrayList<>();
+		for (int i = 0; i < part.length; i++) {
+		    String term = part[i];
+		    double coefficient = 0;
+		    int power = 0;
+		    if (term.contains("x")) {
+		        String[] itemPart = term.split("x");
+		        // The coefficient part.
+		        if (itemPart[0].isEmpty() || itemPart[0].equals("+")) {
+		            coefficient = 1;
+		        } else if (itemPart[0].equals("-")) {
+		            coefficient = -1;
+		        } else {
+		            coefficient = Double.parseDouble(itemPart[0]);
+		        }
+		        // The power part.
+		        if (itemPart.length > 1) {
+		            power = Integer.parseInt(itemPart[1].replace("^", ""));
+		        } else {
+		            power = 1;
+		        }
+		    } else {
+		        coefficient = Double.parseDouble(term);
+		        power = 0;
+		    }
+		    coefficientList.add(coefficient);
+		    powerList.add(power);
+		}
+		this.coefficient = new double[coefficientList.size()];
+        this.power = new int[powerList.size()];
+        for (int i = 0; i < coefficientList.size(); i++) {
+            this.coefficient[i] = coefficientList.get(i);
+            this.power[i] = powerList.get(i);
+        }
+	}
+	
+	public void saveToFile(String fileName) throws IOException {
+	        FileWriter fileWriter = new FileWriter(fileName);
+	        PrintWriter printWriter = new PrintWriter(fileWriter);
+	        for (int i = 0; i < this.coefficient.length; i++) {
+	            double coefficient = this.coefficient[i];
+	            int power = this.power[i];
+	            // Add sign.
+	            if (coefficient > 0 && i != 0) {
+	                printWriter.print("+");
+	            }
+	            // Add coefficient.
+	            if (coefficient != 1 || power == 0) {
+	                printWriter.print(coefficient);
+	            }
+	            // Add power.
+	            if (power != 0) {
+	                printWriter.print("x");
+	                if (power != 1) {
+	                    printWriter.print(exponent);
+	                }
+	            }
+	        }
+	        printWriter.close();
+	    }
     }
 }
